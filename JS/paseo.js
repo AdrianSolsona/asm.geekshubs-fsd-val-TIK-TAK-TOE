fb1c189c18ecd87de04d5ec027d4e8d0c9ea5c35
solsona1008 <solsona1008@gmail.com>
2023-02-06 21:46:54 +0100

    Se añaden edificios en paseo.js y modificación de estilos de las flechas

--- JS/paseo.js
+++ JS/paseo.js
@@ -1,7 +1,10 @@
 
 let personaje = document.getElementById("personaje");
+
 let contador = 0;
 let numeroArboles = 30;
+let numeroEdificios = 7;
+let numeroBolas =10;
 
 function generacionArboles(){
     for(let i = 0; i < numeroArboles; i++){
@@ -13,17 +16,34 @@ function generacionArboles(){
     }
 }
 function generacionPokeballs() {
-    for(let i = 0; i < numeroArboles; i++){
+    for(let i = 0; i < numeroBolas; i++){
         const div = document.createElement('div')
         div.classList.add('pokeball')
-        div.style.left = Math.random() * 100 + '%'
-        div.style.top = Math.random() * 100 + '%'
+        div.style.left = (Math.random()  * 100 + "%")
+        div.style.top = Math.random()  * 100 + "%"
         document.body.appendChild(div)
     }
 }
 
-generacionArboles()
-generacionPokeballs()
+function generacionEdificios(){
+    for(let i = 0; i < numeroEdificios; i++){
+        const div = document.createElement('div')
+        div.classList.add('edificios')
+        div.style.left = (Math.random() * 100 + "%")
+        div.style.top = Math.random() * 100 + "%"
+        div.style.margin = "20px !important"
+        document.body.appendChild(div) 
+    }
+    
+}
+function generacionElementos(){
+    generacionArboles()
+    generacionPokeballs()
+    generacionEdificios()
+}
+
+generacionElementos()
+
 let direccion = "abajo";
 
 let abajo = function(){
@@ -51,10 +71,11 @@ var animacionPersonaje = function(){
     setInterval(function(){
         personaje.style.backgroundPositionX = (50 * contador) + "px" //Cantidad de espacios a la derecha que movemos el personaje para pasar al siguiente sprite
         contador++;
-    },280) //Valor del setInterval cambiara de posicion cada 300 milisegundos(0.3segundos)
+    },250) //Valor del setInterval cambiara de posicion cada 300 milisegundos(0.3segundos)
+
+    
+
 }
-
-
 
 
 let contadorMovimientoVertical = 0;
@@ -64,19 +85,19 @@ let movimientoPersonaje = function(){
     setInterval(function(){
         
         if(direccion == "abajo"){
-            personaje.style.top = (8 * contadorMovimientoVertical) + "px"
+            personaje.style.top = (9 * contadorMovimientoVertical) + "px"
             contadorMovimientoVertical++;
         }
         if(direccion == "arriba"){
-            personaje.style.top = (8 * contadorMovimientoVertical) + "px"
+            personaje.style.top = (9 * contadorMovimientoVertical) + "px"
             contadorMovimientoVertical--;
         }
         if(direccion == "derecha"){
-            personaje.style.left = (8 * contadorMovimientoHorizontal) + "px"
+            personaje.style.left = (9 * contadorMovimientoHorizontal) + "px"
             contadorMovimientoHorizontal++;
         }
         if(direccion == "izquierda"){
-            personaje.style.left = (8 * contadorMovimientoHorizontal) + "px"
+            personaje.style.left = (9 * contadorMovimientoHorizontal) + "px"
             contadorMovimientoHorizontal--;
         }
         if(contadorMovimientoHorizontal <= 0){
@@ -86,13 +107,14 @@ let movimientoPersonaje = function(){
         if(contadorMovimientoVertical <=0){
             contadorMovimientoVertical++;
         }
-        if(contadorMovimientoHorizontal >= 235){
+        if(contadorMovimientoHorizontal >= 209){
             contadorMovimientoHorizontal--;
         }
-        if(contadorMovimientoVertical >= 112){
+        if(contadorMovimientoVertical >= 99){
             contadorMovimientoVertical--
         }
-    },300)
+    },450)
+    
 }
 
 movimientoPersonaje()
