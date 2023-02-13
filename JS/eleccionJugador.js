ae99850b482aad48204da779919f871bfc691c96
adrian salona <Adrian>
2023-02-13 19:08:07 +0100

    Se agrega un pop-up en eleccion.js y se elimina la alerta anterior

--- JS/eleccionJugador.js
+++ JS/eleccionJugador.js
@@ -10,7 +10,27 @@ const seleccionJugador = () => {
     sessionStorage.setItem("Nombre del jugador 2",JSON.stringify(jugador2));
     //Si alguno de los inputs estan vacios, lanzamos alerta a ,los jugadores, sino pasamos al juego
     if(jugador1 == "" || jugador2 == ""){
-        alert("Tienes que poner algun nombre para los 2 jugadores")
+        //alert("Tienes que poner algun nombre para los 2 jugadores")
+        let mensaje = document.createElement("div");
+            mensaje.innerHTML = "¡Tienes que introducir dos nombres válidos!<br><br> Cualquier caracter es válido <br> (Máximo de 20 caracteres)";
+            mensaje.style.width = "30rem"
+            mensaje.style.height = "10rem"
+            mensaje.style.opacity = "0.7"
+            mensaje.style.position = "fixed";
+            mensaje.style.top = "37%";
+            mensaje.style.left = "50%";
+            mensaje.style.transform = "translate(-50%, -50%)";
+            mensaje.style.backgroundColor = "rgb(229, 236, 16)";
+            mensaje.style.fontFamily = "'Mochiy Pop One', sans-serif"
+            mensaje.style.color = "rgb(60, 78, 199)"
+            mensaje.style.textAlign = "center"
+            mensaje.style.padding = "25px";
+            mensaje.style.borderRadius = "7px";
+            document.body.appendChild(mensaje);
+
+            setTimeout(function () {
+                mensaje.style.display = "none";
+              }, 5000);
     }
     else{
         document.getElementById("irJuego").href="../pages/juego.html"
